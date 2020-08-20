246a3713b0b000cc4b1af40e4227414895449cee
iamnxl1798 <lamnx1798@gmail.com>
2020-08-20 10:45:44 +0700

    Cong Thuc Tinh Moi

---
+++ Web/ConsoleApp1/MyCustom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MyCustom
+    {
+        public int DiemDoID { get; set; }
+        public int KenhID { get; set; }
+        public int index { get; set; }
+        public double value { get; set; }
+    }
+}
